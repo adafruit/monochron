d475e6de670db4c3be3672e493fe7768019beaa2
Ladyada <limor@ladyada.net>
2010-01-31 14:23:44 -0500

    Cleaned up, added some comments

--- firmware/util.h
+++ firmware/util.h
@@ -1,3 +1,7 @@
+#include <avr/pgmspace.h>
+
+// Raw constants for the UART to make the bit timing nice
+
 #if (F_CPU == 16000000)
 #define BRRL_9600 103    // for 16MHZ
 #define BRRL_192 52    // for 16MHZ
@@ -6,14 +10,8 @@
 #define BRRL_192 26    
 #endif
 
-#include <avr/pgmspace.h>
-
-#define NOP asm("nop");
+// Debug printing functions - handy!
 #define uart_putc(c) uart_putchar(c)
-
-void delay_ms(unsigned char ms);
-void delay_10us(uint8_t us);
-void delay_s(uint8_t s);
 
 int uart_putchar(char c);
 void uart_init(uint16_t BRR);
@@ -30,6 +28,14 @@ void uart_puts(const char* str);
 void RAM_putstring(char *str);
 void ROM_putstring(const char *str, uint8_t nl);
 
+// by default we stick strings in ROM to save RAM
 #define putstring(x) ROM_putstring(PSTR(x), 0)
 #define putstring_nl(x) ROM_putstring(PSTR(x), 1)
 #define nop asm volatile ("nop\n\t")
+
+// some timing functions
+
+void delay_ms(unsigned char ms);
+void delay_10us(uint8_t us);
+void delay_s(uint8_t s);
+
